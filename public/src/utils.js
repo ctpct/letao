a0fdb72d372153ca67fb14d78024382cd3d5c64b
ctpct <1097276680@qq.com>
2017-11-05 19:23:50 -0400

    登录完成

---
+++ public/src/utils.js
@@ -0,0 +1,23 @@
+define(['jquery'],function($) {
+	$.ajax({
+    	url:'/api/employee/checkRootLogin',
+    	type:'get',
+    	success:function(info) {
+        	if(info.error) {
+            	location.href = '/login.html';
+        	}
+    	}
+    });
+
+    $('.logout').on('click',function() {
+    	$.ajax({
+    		url:'api/employee/employeeLogout',
+    		type:'get',
+    		success:function(info) {
+    			if(info.success) {
+    				location.href = '/login.html';
+    			}
+    		}
+    	})
+    })
+})
